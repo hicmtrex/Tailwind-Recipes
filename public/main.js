c869661f46bde5e1d8ab82be861a61b724c3bd85
Firstname Lastname <your_email@youremail.com>
2022-02-04 21:32:03 +0100

    pushing

---
+++ public/main.js
@@ -0,0 +1,10 @@
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu');
+
+burger.addEventListener('click', () => {
+  if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden');
+  } else {
+    menu.classList.add('hidden');
+  }
+});
